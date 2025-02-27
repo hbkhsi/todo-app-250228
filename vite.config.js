e6c910c9b20dce3f7d1a1fcc6b3efe8e501a20f8
hbkhsi <hbkhosoi0122@gmail.com>
2025-02-28 01:01:20 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'framer-motion'],
+          vendor: ['react', 'react-dom'],
           animations: ['framer-motion']
         }
       }
